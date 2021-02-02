e19f6e48093a8cf7190813bf232e1f6e9d9d61ca
lwampofu <44609816+lwampofu@users.noreply.github.com>
2021-02-02 17:29:18 +0200

    1.2 BOW feature vector file

---
+++ cse408_bow.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 17:14:55 2021
+
+@author: Lwax Malax
+"""
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize
+
+# function to return text as an array
+def feat_vec(filepath,voc):
+    # initial array of the text file given in the path
+    text_arr = []
+    # frequency of each word in the lexicon (voc)
+    frequency = 0
+    # the one-dimensional feature vector 
+    feat_vec = []
+    # Open the file with read only permit
+    f = open(filepath,'r')
+    # use readline() to read the first line 
+    line = f.readline()
+    text_arr.append(line)
+    while line:
+        # use realine() to read next line
+        line = f.readline()
+        text_arr.append(line)
+       
+    f.close()
+    # converting array of words to a string in lower case
+    str1 = " "   
+    list_as_a_string = str1.join(text_arr).lower()
+
+    # Tokenizing the string
+    # This returns a list of tokens
+    text_tokens = word_tokenize(list_as_a_string)    
+
+    for word in voc: 
+        frequency = text_tokens.count(word)
+        feat_vec.append(frequency)
+    #print(feat_vec)
+    return feat_vec
+
+
+
